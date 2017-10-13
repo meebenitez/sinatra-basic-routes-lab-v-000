71796b0a1946fa2e4ed2fecd6c55140708fd9a22
Mary Benitez <meebenitez@gmail.com>
2017-10-13 17:17:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Mary"
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Longest Time"
+  end
+
 end
